086a3bf8b23021cd3fa7559d5d2fbc10627c1018
Daniyal Bhaila <daniyal_1999@hotmail.com>
2025-09-27 00:50:31 -0400

    Refresh marketing sections layout

---
+++ src/lib/imageConfig.ts
@@ -0,0 +1,17 @@
+export const CARD_IMAGE_DIMENSIONS = {
+  width: 960,
+  height: 640,
+  sizes: '(max-width: 1024px) 100vw, 640px',
+};
+
+export const FEATURE_IMAGE_DIMENSIONS = {
+  width: 1200,
+  height: 800,
+  sizes: '(max-width: 1280px) 100vw, 800px',
+};
+
+export const SQUARE_IMAGE_DIMENSIONS = {
+  width: 960,
+  height: 960,
+  sizes: '(max-width: 1024px) 80vw, 480px',
+};
